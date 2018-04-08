a5816604975a51500eaeae81eb8e3342488adc25
MichalNiedziela1601 <michal.niedziela@labnoratory.com>
2018-04-08 11:13:43 +0100

    show recipe ingredients - green

--- src/test/java/com/example/spring5recipes/controllers/IngredientControllerTest.java
+++ src/test/java/com/example/spring5recipes/controllers/IngredientControllerTest.java
@@ -1,11 +1,16 @@
 package com.example.spring5recipes.controllers;
 
+import com.example.spring5recipes.commands.RecipeCommand;
+import com.example.spring5recipes.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -13,17 +18,23 @@ public class IngredientControllerTest {
 
     private IngredientController controller;
 
+    @Mock
+    RecipeService recipeService;
+
     MockMvc mockMvc;
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-        controller = new IngredientController();
+        controller = new IngredientController(recipeService);
 
         mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
 
     @Test
     public void tesIngredientsListPage() throws Exception {
+
+        RecipeCommand command = new RecipeCommand();
+        when(recipeService.findCommandById(anyLong())).thenReturn(command);
 
         mockMvc.perform(get("/recipe/1/ingredients"))
                 .andExpect(status().isOk())
